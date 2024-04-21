9db89a1853faadf0bef0f27f62554fb7be15b748
Daniel Lacerda <daniellacerdaweb@gmail.com>
2024-04-21 11:28:39 -0300

    feat: create request and update atom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+'use client';
+import { atom, useAtom } from 'jotai';
+import { useEffect } from 'react';
+const baseAtom = atom([] as any);
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">teste</main>;
+  const request = async () => fetch('http://localhost:3000/api/burger').then((res) => res.json());
+  const [value, setValue] = useAtom(baseAtom);
+
+  const updateState = async () => {
+    const data = await request();
+    setValue(data);
+  };
+
+  useEffect(() => {
+    updateState();
+  }, []);
+
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">{value[0]?.name}</main>;
 }
